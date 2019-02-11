292a2b233196030980798ad44dabbf82f6f52aa3
Austin Cauley <clikok@aol.com>
2019-02-11 16:28:33 -0600

    Added files used to test movement engine w/ movement

---
+++ src/_test/physicsenginetest/PhysicsVector.java
@@ -0,0 +1,71 @@
+class PhysicsVector{
+    double x, y;
+    private double mass;
+    private int jumpCount = 0;
+    private static final int MAX_JUMP_COUNT = 2;
+
+    public PhysicsVector(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.mass = 1;
+    }
+
+    private double magnitude(double nX, double nY) {
+        return Math.sqrt(nX * nX + nY * nY);
+    }
+
+    private double direction(double nX, double nY) {
+        return Math.atan(nY/nX); //NOTE: The direction will be in radians
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public void normalize() {
+        double nMag = magnitude(x, y);
+        if (nMag == 0) {
+            x = 0;
+            y = 0;
+        } else {
+            x /= nMag;
+            y /= nMag;
+        }
+    }
+
+    public void add(PhysicsVector vector) {
+        x += vector.x;
+        y += vector.y;
+    }
+
+    public void minus(PhysicsVector vector) {
+        x -= vector.x;
+        y -= vector.y;
+    }
+
+    public void mult(double scalar) {
+        x *= scalar;
+        y *= scalar;
+    }
+
+    public void div(double scalar) {
+        x /= scalar;
+        y /= scalar;
+    }
+
+    //Newton's 2nd Law of Motion: Acceleration = Force / Mass
+    public void applyForce(PhysicsVector vector) {
+        vector.div(this.mass);
+        this.add(vector);
+    }
+
+    public PhysicsVector calculateFriction(PhysicsVector velocity) {
+        double coefficientOfFriction = 0.01; //Note: This value depends on the surface
+        double normalForce = 0.1;  //Hardcoded for now, should ideally be calculated
+        double frictionMag = coefficientOfFriction * normalForce;
+        PhysicsVector friction = new PhysicsVector(velocity.x * -1, velocity.y * -1);
+        friction.normalize();
+        friction.mult(frictionMag);
+        return friction;
+    }
+}
