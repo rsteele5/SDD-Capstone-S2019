6a421529d3af5a070ea9274b4b95c24bfdee5484
Greg <dogzilla3@gmail.com>
2019-02-24 21:19:16 -0600

    Fixed options screen

--- src/gamescreens/screens/menus/options/OptionScreen.java
+++ src/gamescreens/screens/menus/options/OptionScreen.java
@@ -25,41 +25,50 @@ public class OptionScreen extends GameScreen {
     @Override
     protected void initializeScreen() {
 
+        //Create Background
+        ImageContainer image;
+        image = (new ImageContainer(0,0, "/assets/backgrounds/BG-OptionMenu.png", DrawLayer.Background));
+        image.addToScreen(this,true);
+
         //Create buttons
-        addObject(new Button(X_INIT_BUTTON,Y_INIT_BUTTON,
+        Button button;
+        button = new Button(X_INIT_BUTTON,Y_INIT_BUTTON,
                 "/assets/buttons/Button-Graphics.png",
                 DrawLayer.Entity,
                 (GameScreen) ->{
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Graphics");
                     screenManager.addScreen(new GraphicsScreen(screenManager));
-                }));
+                });
+        button.addToScreen(this,true);
 
-        addObject(new Button(X_INIT_BUTTON+X_BUFFER+WIDTH_BUTTON,Y_INIT_BUTTON,
+        button = new Button(X_INIT_BUTTON+X_BUFFER+WIDTH_BUTTON,Y_INIT_BUTTON,
                 "/assets/buttons/Button-Sound.png",
                 DrawLayer.Entity,
                 (GameScreen) ->{
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Sound");
                     //TODO: Add Sound Menu
-                }));
+                });
+        button.addToScreen(this,true);
 
-        addObject(new Button(X_INIT_BUTTON+2*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
+        button = new Button(X_INIT_BUTTON+2*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
                 "/assets/buttons/Button-Controls.png",
                 DrawLayer.Entity,
                 (GameScreen) ->{
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Controls");
                     screenManager.addScreen(new ControlsScreen(screenManager));
-                }));
+                });
+        button.addToScreen(this,true);
 
-        addObject(new Button(X_INIT_BUTTON+3*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
+        button = new Button(X_INIT_BUTTON+3*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
                 "/assets/buttons/Button-MainMenu.png",
                 DrawLayer.Entity,
                 (GameScreen) ->{
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Main Menu");
                     this.setScreenState(ScreenState.TransitionOff);
-                }));
+                });
+        button.addToScreen(this,true);
 
-        //Create Background
-        addObject(new ImageContainer(0,0, "/assets/backgrounds/BG-OptionMenu.png", DrawLayer.Background));
+
     }
 
     //endregion
