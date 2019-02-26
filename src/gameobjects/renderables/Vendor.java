12cf4dafd577a6d0967d710f27590354e7c81966
Gina Quinn <mdwgina@gmail.com>
2019-02-25 20:53:49 -0600

    Starting to redo vendor

---
+++ src/gameobjects/renderables/Vendor.java
@@ -0,0 +1,14 @@
+package gameobjects.renderables;
+
+public class Vendor extends RenderableObject {
+
+    // Default constructor
+    public Vendor(){
+        super();
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
