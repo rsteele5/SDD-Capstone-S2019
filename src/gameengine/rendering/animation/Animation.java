09c7fafcd3ed564961d985c2af8d7dfafc1fcc8d
Greg <dogzilla3@gmail.com>
2019-03-01 15:39:39 -0600

    Animator works

---
+++ src/gameengine/rendering/animation/Animation.java
@@ -0,0 +1,29 @@
+package gameengine.rendering.animation;
+
+import main.utilities.Loadable;
+
+import java.util.ArrayList;
+
+public abstract class Animation implements Loadable {
+
+    protected ArrayList<AnimationImage> animationImages = new ArrayList<>();
+    protected int frameToDisplay = -1;
+    protected int totalFrames = 0;
+
+    public void addAnimationImage(AnimationImage image){
+        animationImages.add(image);
+        totalFrames++;
+    }
+
+    public AnimationImage next(){
+        frameToDisplay++;
+        if(frameToDisplay >= animationImages.size()){  frameToDisplay = 0; }
+        return animationImages.get(frameToDisplay);
+    }
+
+    public void load(){
+        for (AnimationImage animationImage: animationImages){
+            animationImage.load();
+        }
+    }
+}
