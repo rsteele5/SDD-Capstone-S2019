5f5d1ff826ab9348219a765980e17b3c9aff2b7c
Mari Miller <redreaper33@gmail.com>
2019-03-02 03:54:38 -0600

    Revert "Messed with the ButtonText a bit.  Yes btw, I know the vendor turns all the buttons black."
    This reverts commit f83ece7b7bd0c0218504325d3abe2f30589b06ea.

--- src/gamescreens/screens/menus/dev/DevScreen.java
+++ src/gamescreens/screens/menus/dev/DevScreen.java
@@ -3,7 +3,6 @@ package gamescreens.screens.menus.dev;
 import _test.TestDialogBoxScreen;
 import gameobjects.renderables.TempPlayerClass;
 import gameobjects.renderables.Vendor;
-import gameobjects.renderables.buttons.ButtonText;
 import gamescreens.DrawLayer;
 import gamescreens.GameScreen;
 import gameobjects.renderables.ImageContainer;
@@ -16,8 +15,6 @@ import gamescreens.screens.Level;
 import gamescreens.screens.VendorScreen;
 import main.utilities.Debug;
 import main.utilities.DebugEnabler;
-
-import java.awt.*;
 
 
 public class DevScreen extends GameScreen {
@@ -49,52 +46,62 @@ public class DevScreen extends GameScreen {
         imageContainer.addToScreen(this, true);
 
         //Create buttons
-        Font noScary = new Font("NoScary", Font.PLAIN, 70);
-        Color color = Color.WHITE;
-        String imagePath = "/assets/buttons/Button-Empty.png";
-        ButtonText button;
+        Button button;
 
-        button = new ButtonText(X_INIT_BUTTON,Y_INIT_BUTTON - 128,
-                imagePath, DrawLayer.Entity, noScary, "TEST", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Test");
-            screenManager.addScreen(new TestDialogBoxScreen(screenManager)); });
+        button = new Button(X_INIT_BUTTON,Y_INIT_BUTTON - 128,
+                "/assets/buttons/Button-Test.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Test");
+                    screenManager.addScreen(new TestDialogBoxScreen(screenManager));
+                });
         button.addToScreen(this, true);
 
-        button = new ButtonText(X_INIT_BUTTON+(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON - 128,
-                imagePath, DrawLayer.Entity, noScary, "New Game", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Level");
-            screenManager.addScreen(LevelManager.create(screenManager, new BedroomLevel())); });
+        button = new Button(X_INIT_BUTTON+(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON - 128,
+                "/assets/buttons/Button-NewGame.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Level");
+                    screenManager.addScreen(LevelManager.create(screenManager, new BedroomLevel()));
+                });
         button.addToScreen(this, true);
 
-        button = new ButtonText(X_INIT_BUTTON,Y_INIT_BUTTON,
-                imagePath, DrawLayer.Entity, noScary, "Vendor", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Vendor");
-            screenManager.addScreen(new VendorScreen(screenManager)); });
+        button = new Button(X_INIT_BUTTON,Y_INIT_BUTTON,
+                "/assets/buttons/Button-Vendor.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Vendor");
+                    screenManager.addScreen(new VendorScreen(screenManager));
+                });
         button.addToScreen(this, true);
 
-        button = new ButtonText(X_INIT_BUTTON+X_BUFFER+WIDTH_BUTTON,Y_INIT_BUTTON,
-                imagePath, DrawLayer.Entity, noScary, "Physics", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Physics");
-            exiting = true;
-            screenManager.addScreen(new Level(screenManager)); });
+        button = new Button(X_INIT_BUTTON+X_BUFFER+WIDTH_BUTTON,Y_INIT_BUTTON,
+                "/assets/buttons/Button-Physics.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Physics");
+                    exiting = true;
+                    screenManager.addScreen(new Level(screenManager));
+                });
         button.addToScreen(this, true);
 
-        button = new ButtonText(X_INIT_BUTTON+2*(X_BUFFER+WIDTH_BUTTON), Y_INIT_BUTTON,
-                imagePath, DrawLayer.Entity, noScary, "Inventory", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Inventory");
-            screenManager.addScreen(new InventoryScreen(screenManager)); });
+        button = new Button(X_INIT_BUTTON+2*(X_BUFFER+WIDTH_BUTTON), Y_INIT_BUTTON,
+                "/assets/buttons/Button-Inventory.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Inventory");
+                    //TODO: Add Inventory Screen
+                    screenManager.addScreen(new InventoryScreen(screenManager));
+                });
         button.addToScreen(this, true);
 
-        button = new ButtonText(X_INIT_BUTTON+3*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
-                imagePath, DrawLayer.Entity, noScary, "MainMenu", color);
-        button.setOnClick(() ->{
-            Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Main Menu");
-            setScreenState(ScreenState.TransitionOff); });
+        button = new Button(X_INIT_BUTTON+3*(X_BUFFER+WIDTH_BUTTON),Y_INIT_BUTTON,
+                "/assets/buttons/Button-MainMenu.png",
+                DrawLayer.Entity,
+                () ->{
+                    Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Main Menu");
+                    setScreenState(ScreenState.TransitionOff);
+                });
         button.addToScreen(this, true);
 
         // Initialize vendor
